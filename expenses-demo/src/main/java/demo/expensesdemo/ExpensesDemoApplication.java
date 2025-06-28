1253fa056dca32a14c42c0daf2084efea444d1b5
Robu Andrei <robu.andrew@gmail.com>
2025-06-28 02:49:24 -0400

    test

---
+++ expenses-demo/src/main/java/demo/expensesdemo/ExpensesDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.expensesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesDemoApplication.class, args);
+    }
+
+}
